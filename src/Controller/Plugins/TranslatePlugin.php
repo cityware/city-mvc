733ed00ab10cfab42198a9dc43a16ef82ad1b242
Fabricio Valadares <valadares.fabricio@gmail.com>
2015-12-02 10:01:13 -0200

    Update

--- src/Controller/Plugins/TranslatePlugin.php
+++ src/Controller/Plugins/TranslatePlugin.php
@@ -3,28 +3,33 @@
 namespace Cityware\Mvc\Controller\Plugins;
 
 use Zend\Mvc\Controller\Plugin\AbstractPlugin;
-use Zend\Mvc\I18n\Translator;
+use Zend\I18n\Translator\Translator;
 
 class TranslatePlugin extends AbstractPlugin {
 
     /** @var  Translator $translator */
-    private $translator;
+    private $globalConfig;
 
     /**
      * TranslatePlugin constructor.
      * @param Translator $translator
      */
-    public function __construct(Translator $translator) {
-        $this->translator = $translator;
+    public function __construct() {
+        //$this->globalConfig = \Zend\Config\Factory::fromFile(GLOBAL_CONFIG_PATH . 'global.php');
     }
 
     /**
      * @param $str
-     * @param null $locale
+     * @param pt_BR $locale
      * @return string
      */
-    public function __invoke($str, $locale = null) {
-        return $locale ? $this->translator->translate($str, $locale) : $this->translator->translate($str);
+    public function __invoke($str, $locale = 'pt_BR', $typeTranslate = 'PhpArray', $pathFileTranslate = null) {
+        $translator = new Translator();
+
+        if (!empty($pathFileTranslate)) {
+            $translator->addTranslationFile($typeTranslate, $pathFileTranslate, 'default', $locale);
+        }
+        return $translator->translate($str, $locale);
     }
 
 }
